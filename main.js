befd362714bee01b8f7703912d7a3fa00e8e29ea
qlu223 <clulelss223@gmail.com>
2025-04-20 20:28:32 -0500

    data displays onto app

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
+const isDev = process.env.NODE_ENV !== 'development';
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+fs.createReadStream("./data/prem_table.csv")
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    console.log(row);
   })
+  .on("end", function () {
+    console.log("finished");
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+const createWindow = () => {
+    const win = new BrowserWindow({
+        title: 'SportsDashboard',
+        width: isDev ? 1000 : 500,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+            }
+    })
+    if(isDev){
+        win.webContents.openDevTools()  
+    }
 
-  win.loadFile('index.html')
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
 
-  app.on('activate', () => {
+    app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
