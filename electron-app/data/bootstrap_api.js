3ced810fc1233544c134ee44c35d26379f65fad6
albertezem <ezemosakwea@carleton.edu>
2025-05-08 13:53:49 -0500

    changes in progress

--- electron-app/data/bootstrap_api.js
+++ electron-app/data/bootstrap_api.js
@@ -18,7 +18,7 @@ class BootstrapAPI {
     }
     async getLeagueData() {
         let data = await this.fetchData();
-        //console.log(data['teams'])
+        console.log(data['teams'])
         return data['teams'];
     }
 
@@ -28,9 +28,22 @@ class BootstrapAPI {
         return data['elements'];
     }
 
-    
+    async getFilteredLeagueData() {
+        let data = await this.getLeagueData()
+
+        let table = []
+
+        for (let i in data) {
+            let row = data[i];
+            let new_row = { code: row.code, name: row.name, pts: row.points };
+
+            table.push(new_row)
+        }
+        console.log(table)
+        return table;
+    }
 }
 
 let t = new BootstrapAPI;
-t.getPlayerData()
+t.getFilteredLeagueData()
 
