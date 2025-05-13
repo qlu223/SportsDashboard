60b7ac24e0ecf246226042e809bebfe5ef88c1a0
qlu223 <clulelss223@gmail.com>
2025-05-13 17:52:24 -0500

    edits

--- electron-app/test/specs/test.e2e.ts
+++ electron-app/test/specs/test.e2e.ts
@@ -15,27 +15,31 @@ describe('Tab Click Testing', () =>{
         await myButton.click() 
     })
     it('should demonstrate a click on the teams page', async () => {
-        const myButton = await $('#vertical-tab-2')
-        await myButton.click() 
-    })
-    it('should demonstrate a click on the visualizations page', async () => {
         const myButton = await $('#vertical-tab-3')
         await myButton.click() 
     })
-    it('should demonstrate a click on the predictions page', async () => {
+    it('should demonstrate a click on the visualizations page', async () => {
         const myButton = await $('#vertical-tab-4')
         await myButton.click() 
     })
-})
-describe('Filter click testing', ()=>{
-    it('should demonstrate a click on the view filter',async ()=>{
-        const myButton = await $('#view-select')
-        await myButton.click()
-    })
-    it('should demonstrate a click on the sortedby filter',async ()=>{
-        const myButton = await $('#sort-by-select')
-        await myButton.click()
+    it('should demonstrate a click on the predictions page', async () => {
+        const myButton = await $('#vertical-tab-5')
+        await myButton.click() 
     })
 })
+// describe('Filter click testing', ()=>{
+//     it('should demonstrate a click on the teams page', async () => {
+//         const myButton = await $('#vertical-tab-3')
+//         await myButton.click() 
+//     })
+//     it('should demonstrate a click on the view filter',async ()=>{
+//         const myButton = await $('#view-select')
+//         await myButton.click()
+//     })
+//     it('should demonstrate a click on the sortedby filter',async ()=>{
+//         const myButton = await $('#sort-by-select')
+//         await myButton.click()
+//     })
+// })
 
 
