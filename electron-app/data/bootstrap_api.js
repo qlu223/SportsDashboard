62dbd66b75d09d709d3e37a90bd88e082e4b58ec
albertezem <ezemosakwea@carleton.edu>
2025-05-13 15:10:08 -0500

    tests for bootstrap_api.js

--- electron-app/data/bootstrap_api.js
+++ electron-app/data/bootstrap_api.js
@@ -1,6 +1,9 @@
-class BootstrapAPI {
+export default class BootstrapAPI {
     url = "https://fantasy.premierleague.com/api/bootstrap-static/";
-
+    /**
+     * Retrieves data from bootstrap-static URL
+     * @returns A JSON response
+     */
     async fetchData() {
         try {
             let response = await fetch(this.url);
@@ -16,21 +19,24 @@ class BootstrapAPI {
             console.error("Error fetching data:", error);
         }
     }
+    /**
+     * Retrieves team data from API
+     * @returns an array of dictionaries, each dictionary referring to a particular team
+     */
     async getLeagueData() {
         let data = await this.fetchData();
         //console.log(data['teams'])
         return data['teams'];
     }
-
+    /**
+     * Retrieves player data from API
+     * @returns an array of dictionaries, each dictionary referring to a particular player
+     */
     async getPlayerData() {
         let data = await this.fetchData();
         //console.log(data['elements'])
         return data['elements'];
     }
-
-    
 }
 
-let t = new BootstrapAPI;
-t.getPlayerData()
 
