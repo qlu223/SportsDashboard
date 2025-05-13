2e508373013a097e90b3f5816f6df983a8d49f16
qlu223 <clulelss223@gmail.com>
2025-05-13 16:17:29 -0500

    idk

--- electron-app/src/app.jsx
+++ electron-app/src/app.jsx
@@ -2,24 +2,11 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import VerticalTabs from './Tabs.jsx';
-import { AppProvider } from '@toolpad/core/AppProvider';
-import { SignInPage } from '@toolpad/core/SignInPage';
-
 
   
 function App(){
     return (
         <div>
-            {/* <AppProvider>
-                <SignInPage 
-                    providers = {[
-                        { id: 'github', name: 'GitHub' },
-                        { id: 'google', name: 'Google' },]}
-                    signIn={async (provider)=>{
-
-                    }}
-                />
-            </AppProvider> */}
             <VerticalTabs />
         </div>
     );
