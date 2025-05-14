0e385eda5765f449d979ddcde5c343784d21f040
albertezem <ezemosakwea@carleton.edu>
2025-05-13 23:41:23 -0500

    added filtered league table

--- electron-app/data/bootstrap_api.js
+++ electron-app/data/bootstrap_api.js
@@ -18,7 +18,7 @@ export default class BootstrapAPI {
     }
     async getLeagueData() {
         let data = await this.fetchData();
-        console.log(data['teams'])
+        //console.log(data['teams'])
         return data['teams'];
     }
 
@@ -35,7 +35,7 @@ export default class BootstrapAPI {
 
         for (let i in data) {
             let row = data[i];
-            let new_row = { code: row.code, name: row.name, pts: row.points };
+            let new_row = { code: row.code, name: row.name, win: row.win, draw: row.draw, loss: row.loss, pts: row.points };
 
             table.push(new_row)
         }
--- electron-app/data/fixtures.js
+++ electron-app/data/fixtures.js
@@ -19,10 +19,24 @@ class FixturesAPI{
     }
     async getFixturesData() {
         let data = await this.fetchData();
-        console.log(data[0])
+        //console.log(data[0])
         return data;
+    }
+    async getFilteredFixturesData() {
+        let data = await this.getFixturesData();
+        
+        let table = []
+
+        for (let i in data) {
+            let row = data[i];
+            let new_row = { code: row.code, team_h: row.team_h, team_a: row.team_a, time: row.kickoff_time, team_h_score: row.team_h_score, team_a_score: row.team_a_score};
+
+            table.push(new_row)
+        }
+        //console.log(table)
+        return table;
     }
 }
 
 let t = new FixturesAPI;
-t.getFixturesData()
+t.getFilteredFixturesData()
