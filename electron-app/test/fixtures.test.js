9bed26922a3663069a2020396a6067ef575885c1
albertezem <ezemosakwea@carleton.edu>
2025-05-19 11:17:09 -0500

    fixtures test

--- electron-app/test/fixtures.test.js
+++ electron-app/test/fixtures.test.js
@@ -3,5 +3,10 @@ import { describe, it, expect, vi } from 'vitest';
 import FixturesAPI from '../data/fixtures.js';
 
 describe('FixturesAPI', () => {
+    it('returns fixture object', async () => {
+        let t = new FixturesAPI;
+        let fixtures = await t.getFixturesData;
 
+        expect(fixtures.length).toBe(380);
+    });
 });
